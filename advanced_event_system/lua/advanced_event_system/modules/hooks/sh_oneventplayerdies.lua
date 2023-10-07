562b3212f667b1f454ac3ea8d47e1b169e45b331
Lord Sugar <62381889+lord-sugarv2@users.noreply.github.com>
2023-10-07 13:10:04 +0100

    Delete advanced_event_system/lua directory

--- advanced_event_system/lua/advanced_event_system/modules/hooks/sh_oneventplayerdies.lua
+++
@@ -1,5 +0,0 @@
-local MODULE = {}
-MODULE.ID = "HOOK:OnEventPlayerDies" -- THIS MUST BE UNIQUE
-MODULE.Name = "when an event player dies" -- Nice text that gets displayed to represent the ID
-
-AEvent:CreateHook(MODULE)
